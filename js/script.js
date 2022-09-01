44fb072c43642ad2ce22338c8d8aabdd690cb07e
Ahsan Habib <ahsanpub16b@gmail.com>
2022-09-01 13:44:24 +0600

    added tasks

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
-console.log("Added!")
+
+//Define Ui Element
+
+let form = document.querySelector('#task_form');
+let taskList = document.querySelector('ul');
+let clearBtn = document.querySelector('#clear_task_btn');
+let filter = document.querySelector('#task_filter');
+let taskInput = document.querySelector('#new_task');
+
+// Define event listeners
+
+form.addEventListener('submit',addTask);
+
+
+//define add task function()
+function addTask(e){
+    if(taskInput.value === ''){
+        alert('Please! Add a Task');
+    } else{
+        //create li-list element
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(taskInput.value + " "));
+
+        //link added to li element
+        let link = document.createElement('a');
+        link.setAttribute('href' , '#');
+        link.innerHTML =  'X' ; 
+        li.appendChild(link);
+
+        taskList.appendChild(li);
+
+        //clear that input value
+        taskInput.value = '';
+
+    }
+    e.preventDefault();
+}
+
+
